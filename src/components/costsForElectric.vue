7d018be3134bb2f113f827ad935c7faa25e5ef08
Linards <linards_lazdins@hotmail.com>
2021-04-08 14:54:06 +0300

    added option to compare with EV

---
+++ src/components/costsForElectric.vue
@@ -0,0 +1,81 @@
+<template>
+	<div class="container mt-3 p-4 card">
+		<div class="close-btn">
+			<button @click="$emit('toggle-ev')" type="button" class="btn-close" aria-label="Close"></button>
+		</div>
+		<label for="energyConsumption" class="form-label text-center w-100"
+			>Average energy consumption : <strong>{{ energyConsumption }} Wh/km.</strong></label
+		>
+		<input
+			v-model="energyConsumption"
+			type="range"
+			class="form-range"
+			min="1"
+			max="1000"
+			step="1"
+			id="energyConsumption"
+		/>
+		<label for="pricePerWh" class="form-label text-center w-100"
+			>Price per kWh: <strong>{{ pricePerWh }} EUR.</strong></label
+		>
+		<input v-model="pricePerWh" type="range" class="form-range" min="0.01" max="1" step="0.0001" id="pricePerWh" />
+		<p class="text-center" v-show="costPerKm">
+			1 km costs <strong>~ {{ costPerKm }} EUR.</strong>
+		</p>
+		<label for="calculateYourTripCosts" class="form-label text-center w-100"
+			>Trip: <strong>{{ trip }} km.</strong></label
+		>
+		<input
+			v-model="trip"
+			@change="$emit('trip-change', trip)"
+			type="range"
+			class="form-range"
+			min="1"
+			max="2500"
+			step="1"
+			id="calculateYourTripCosts"
+		/>
+		<p class="text-center mb-0">
+			This trip will cost you <strong>~ {{ pricePerTrip }} EUR.</strong>
+		</p>
+	</div>
+</template>
+<script>
+export default {
+	name: "CalculateCost",
+	props: ["trip"],
+	data() {
+		return {
+			energyConsumption: 165,
+			pricePerWh: 0.1045,
+		};
+	},
+	computed: {
+		costPerKm() {
+			return ((this.energyConsumption / 1000) * this.pricePerWh).toFixed(3);
+		},
+		pricePerTrip() {
+			return (this.costPerKm * this.trip).toFixed(2);
+		},
+	},
+};
+</script>
+<style scoped>
+.card {
+	background-color: #ecf0f1;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	max-width: 760px;
+	margin: 0 auto;
+	border-radius: 5px;
+}
+.close-btn {
+	display: flex;
+	justify-content: flex-end;
+	padding-bottom: 0.5rem;
+}
+@media (max-width: 768px) {
+	.card {
+		max-width: 90%;
+	}
+}
+</style>
---
+++ src/components/costsForFossilFuel.vue
@@ -0,0 +1,75 @@
+<template>
+	<div class="container mt-3 p-4 card">
+		<label for="fuelConsumption" class="form-label text-center w-100"
+			>Average fuel consumption : <strong>{{ fuelConsumption }} l/100 km.</strong></label
+		>
+		<input v-model="fuelConsumption" type="range" class="form-range" min="1" max="20" step="0.1" id="fuelConsumption" />
+		<label for="pricePerLiter" class="form-label text-center w-100"
+			>Price per liter: <strong>{{ pricePerLiter }} EUR.</strong></label
+		>
+		<input v-model="pricePerLiter" type="range" class="form-range" min="0.01" max="3" step="0.01" id="pricePerLiter" />
+		<p class="text-center" v-show="costPerKm">
+			1 km costs <strong>~ {{ costPerKm }} EUR.</strong>
+		</p>
+		<label for="calculateYourTripCosts" class="form-label text-center w-100"
+			>Trip: <strong>{{ trip }} km.</strong></label
+		>
+		<input
+			v-model="trip"
+			@change="$emit('trip-change', trip)"
+			type="range"
+			class="form-range"
+			min="1"
+			max="2500"
+			step="1"
+			id="calculateYourTripCosts"
+		/>
+		<p class="text-center">
+			This trip will cost you <strong>~ {{ pricePerTrip }} EUR.</strong>
+		</p>
+		<div class="button-bar text-center">
+			<button v-show="!text" @click="$emit('toggle-ev')" class="btn btn-success">
+				{{ text ? "" : "Compare with EV" }}
+			</button>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: "CalculateCost",
+	props: ["trip", "text"],
+	data() {
+		return {
+			fuelConsumption: 7,
+			pricePerLiter: 1.2,
+		};
+	},
+	computed: {
+		costPerKm() {
+			return ((1 / 100) * this.fuelConsumption * this.pricePerLiter).toFixed(3);
+		},
+		pricePerTrip() {
+			return ((this.trip / 100) * this.fuelConsumption * this.pricePerLiter).toFixed(2);
+		},
+	},
+	methods: {
+		showCalculationsForEv() {
+			this.$emit("toggle-ev", this.trip);
+		},
+	},
+};
+</script>
+<style scoped>
+.card {
+	background-color: #ecf0f1;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	max-width: 760px;
+	margin: 0 auto;
+	border-radius: 5px;
+}
+@media (max-width: 768px) {
+	.card {
+		max-width: 90%;
+	}
+}
+</style>
